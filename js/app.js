158917df193b7f69a5728daac4032d4036cff74f
Em774 <e.dussaix74@gmail.com>
2017-07-04 16:39:18 +0100

    added objects boss attack elements and refactoring

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // create a start button
-  //create a div, make it clickable, link it to the next stage.
+//create a div, make it clickable, link it to the next stage.
 //after the start button, 2 characters should be proposed to chose.
 //(hover on one character to see their criteria?)
 //click on one character to start the game
@@ -10,7 +10,7 @@
 //create one icon for healing
 //click on one icon to activate a spell/attack
 //every three turns, the boss attack from a random array of attacks. =>
-//create an array of attacks
+//create an array of attacks and counter
 //when healthbar of player is empty: you lose (vise versa)
 //display a window central : you lose. restart:RETRY
 //go back to character chosing phase.
@@ -37,6 +37,8 @@ function init() {
   const bar           = $('.bar');
   const bar2          = $('.bar2');
   const $result       = $('.result');
+  const $restart      = $('.restart');
+  let counter         = 2;
 
   $character.hide();
   $displayChar1.hide();
@@ -48,6 +50,7 @@ function init() {
   bar.hide();
   bar2.hide();
   $result.hide();
+  $restart.hide();
 
 
   $startButton.on('click', function() {
@@ -96,55 +99,84 @@ function init() {
   function actionButton() {
     //first attack
     $attack1.on('click', function() {
-      var total = hBar2.data('total');
-      var value = hBar2.data('value');
-
-      if (value <= 0) {
-        $result.show('fast');
-        $result.html('You win!');
-      }
-
-      //damage = 20points
-      var damage   = 20;
-      //result= 80
-      var newValue = value - damage;
-      //in percentage 80%
-      var barWidth = (newValue / total) * 100;
-
-      hBar2.data('value', newValue);
-
-      setTimeout(function(){
-        bar2.css('width', barWidth + '%');
-      }, 500);
-      console.log(value, damage, newValue);
+      var damage = 10;
+      damageHealthBar(damage, hBar2, bar2);
+      checkBossAttack();
     });
 
-    //attack2
+    //secondAttack
     $attack2.on('click', function() {
-      var total = hBar2.data('total');
-      var value = hBar2.data('value');
-
-      if (value <= 0) {
-        $result.show('fast');
-        $result.html('You win!');
-      }
-
-      //damage = 20points
-      var damage   = 50;
-      //result= 80
-      var newValue = value - damage;
-      //in percentage 80%
-      var barWidth = (newValue / total) * 100;
-
-      hBar2.data('value', newValue);
-
-      setTimeout(function(){
-        bar2.css('width', barWidth + '%');
-      }, 500);
-      console.log(value, damage, newValue);
+      var damage = 50;
+      damageHealthBar(damage, hBar2, bar2);
+      checkBossAttack();
     });
 
+    //heal
+    $heal.on('click', function(){
+      hBar.data('value', hBar.data('total'));
+      bar.css('width', '100%');
+    });
   }
 
+  function checkBossAttack() {
+    console.log(counter);
+    if (counter === 0) {
+      bossAttack();
+      counter = 2;
+    } else {
+      counter--;
+    }
 
+    function bossAttack() {
+      var bossDamage = [
+        {
+          damage: '20',
+          heal: '2'
+        },
+        {
+          damage: '30',
+          heal: '5',
+          refresh: '2'
+        },
+        {
+          damage: '50',
+          heal: '10',
+          refresh: '3'
+        },
+        {
+          damage: '70',
+          heal: '5',
+          refresh: '5'
+        }
+      ];
+      var dam = bossDamage[Math.floor(Math.random() * bossDamage.length)].damage;
+      // console.log(dam);
+      setTimeout(function(){
+        damageHealthBar(dam, hBar, bar);
+      }, 1000);
+    }
+  }
+
+  function damageHealthBar(damage, healthbar, subBar) {
+    var total = healthbar.data('total');
+    var value = healthbar.data('value');
+    var newValue = value - damage;
+    var barWidth = (newValue / total) * 100;
+    healthbar.data('value', newValue);
+    setTimeout(function(){
+      subBar.css('width', barWidth + '%');
+    }, 500);
+    winCheck(newValue);
+  }
+
+  function winCheck(newValue){
+    if (newValue <= 0) {
+      setTimeout(function() {
+        $result.show('easing');
+        $result.html('You win!');
+      }, 1000);
+      $restart.show('slow');
+      restart();
+    }
+  }
 }
