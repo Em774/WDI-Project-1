73e19e1e551d1c713cafb9b593220dd6b715378d
Em774 <e.dussaix74@gmail.com>
2017-07-05 14:23:39 +0100

    get more functions into the game

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@
 //click on one icon to activate a spell/attack
 //every three turns, the boss attack from a random array of attacks. =>
 //create an array of attacks and counter
+//display the value of the healthbar above the healthbar.
 //when healthbar of player is empty: you lose (vise versa)
 //display a window central : you lose. restart:RETRY
 //go back to character chosing phase.
@@ -28,10 +29,13 @@ function init() {
   const $displayChar1 = $('#displayChar1');
   const $displayChar2 = $('#displayChar2');
   const $displayBoss  = $('#displayBoss');
+  const $countDown    = $('.countDown');
   const $action       = $('.action');
   const $attack1      = $('#attack1');
   const $attack2      = $('#attack2');
-  const $heal         = $('#heal');
+  const $span1        = $('.span1');
+  const $span2        = $('.span2');
+  const $potion       = $('#potion');
   const hBar          = $('.healthbar');
   const hBar2         = $('.healthbar2');
   const bar           = $('.bar');
@@ -39,12 +43,23 @@ function init() {
   const $result       = $('.result');
   const $restart      = $('.restart');
   let counter         = 2;
+  let attackCounter   = 0;
+  let refresh         = 0;
+
+  //animation
+  let step            = 0;
+  // let x               = 0;
+  let idleZombie;
+  let idleNinja;
 
   $character.hide();
   $displayChar1.hide();
   $displayChar2.hide();
   $displayBoss.hide();
+  $countDown.hide();
   $action.hide();
+  $span1.hide();
+  $span2.hide();
   hBar.hide();
   hBar2.hide();
   bar.hide();
@@ -79,6 +94,8 @@ function init() {
     $displayChar1.show();
     $displayBoss.show();
     $action.show();
+    $span1.show();
+    $span2.show();
     hBar.show();
     hBar2.show();
     bar.show();
@@ -89,37 +106,106 @@ function init() {
     $displayChar2.show();
     $displayBoss.show();
     $action.show();
+    $span1.show();
+    $span1.html('100 / 100');
+    $span2.show();
+    $span2.html('100 / 100');
     hBar.show();
     hBar2.show();
     bar.show();
     bar2.show();
-    actionButton();
+    idleBoss();
+    idleNinjaa();
+    countDown();
+  }
+
+  function countDown() {
+    $countDown.show();
+    var count = 3;
+    var timer = setInterval(function() {
+      handleTimer(count);
+    }, 1000);
+
+    function handleTimer(){
+      if(count === 0) {
+        $countDown.html('Fight!');
+        clearInterval(timer);
+        setTimeout(function() {
+          $countDown.hide();
+          actionButton();
+        }, 1000);
+
+      } else {
+        $countDown.html(count);
+        count--;
+      }
+    }
+  }
+
+
+  function idleBoss() {
+    idleZombie = setInterval(function() {
+      $displayBoss.css('background-position', step * 430);
+      step = (step + 1) % 15;
+    }, 250);
+  }
+
+  function idleNinjaa() {
+    idleNinja = setInterval(function() {
+      $displayChar2.css('background-position', step * 232);
+      step = (step + 1) % 10;
+    }, 180);
   }
 
   function actionButton() {
+
     //first attack
     $attack1.on('click', function() {
       var damage = 10;
       damageHealthBar(damage, hBar2, bar2);
       checkBossAttack();
+      attackCounter ++;
+      checkTurns();
     });
 
     //secondAttack
-    $attack2.on('click', function() {
+    $attack2.on('click', reUse);
+
+    function reUse(){
       var damage = 50;
       damageHealthBar(damage, hBar2, bar2);
       checkBossAttack();
-    });
+      attackCounter ++;
+      $attack2.off();
+      checkTurns();
+    }
+
+    function checkTurns() {
+      if ($attack2.off()) {
+        $attack2.css('background', 'red');
+        refresh ++;
+        console.log(refresh);
+      }
+      if (refresh === 4){
+        $attack2.on('click', reUse);
+        $attack2.css('background', 'green');
+        refresh = 0;
+      }
+
+    }
 
     //heal
-    $heal.on('click', function(){
+    $potion.on('click', function(){
       hBar.data('value', hBar.data('total'));
       bar.css('width', '100%');
+      $span1.html(hBar.data('value') + ' / 100');
+      checkBossAttack();
+      attackCounter ++;
+      checkTurns();
     });
   }
 
   function checkBossAttack() {
-    console.log(counter);
     if (counter === 0) {
       bossAttack();
       counter = 2;
@@ -131,28 +217,31 @@ function init() {
       var bossDamage = [
         {
           damage: '20',
-          heal: '2'
+          heal: '20'
         },
         {
           damage: '30',
-          heal: '5',
+          heal: '10',
           refresh: '2'
         },
         {
           damage: '50',
-          heal: '10',
+          heal: '5',
           refresh: '3'
         },
         {
           damage: '70',
-          heal: '5',
+          heal: '10',
           refresh: '5'
         }
       ];
       var dam = bossDamage[Math.floor(Math.random() * bossDamage.length)].damage;
-      // console.log(dam);
+      var heal = bossDamage[Math.floor(Math.random() * bossDamage.length)].heal;
+
       setTimeout(function(){
         damageHealthBar(dam, hBar, bar);
+        healing(heal, hBar2, bar2); //new
+        $span1.html(hBar.data('value') + ' / 100');
       }, 1000);
     }
   }
@@ -165,6 +254,7 @@ function init() {
     healthbar.data('value', newValue);
     setTimeout(function(){
       subBar.css('width', barWidth + '%');
+      $span2.html(newValue + (' / 100'));
     }, 500);
     winCheck(newValue);
   }
@@ -174,9 +264,28 @@ function init() {
       setTimeout(function() {
         $result.show('easing');
         $result.html('You win!');
+        $restart.show('slow');
       }, 1000);
-      $restart.show('slow');
       restart();
     }
+  }
+
+  function healing(heal, hBar2, bar2 ) {
+    var value = hBar2.data('value');
+    var newValue = +value + +heal;
+    console.log(value, newValue, heal);
+    var barWidth = newValue;
+    hBar2.data('value', newValue);
+    // console.log(newValue);
+    setTimeout(function(){
+      bar2.css('width', barWidth + '%');
+    }, 1000);
+  }
+
+  function restart() {
+    $restart.on('click', function() {
+      location.reload();
+    });
+
   }
 }
